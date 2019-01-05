d3e4172c230a1f6c9dd52633d171a078c973cbef
MROS <yc1043@gmail.com>
2019-01-06 01:07:12 +0800

    預處理斷句

--- pre_process.ts
+++ pre_process.ts
@@ -1,84 +1,19 @@
 import * as fs from "fs";
-import { 注音表示類, 注音種類列舉, toneCharToEnum } from "./basic"
+import {
+    isValidBopomofo,
+    normalizeBopomofo
+} from "./basic"
 
-const 聲母集 = new Set<string>([
-    "ㄅ", "ㄆ", "ㄇ", "ㄈ", "ㄉ",
-    "ㄊ", "ㄋ", "ㄌ", "ㄍ", "ㄎ",
-    "ㄏ", "ㄐ", "ㄑ", "ㄒ", "ㄓ",
-    "ㄔ", "ㄕ", "ㄖ", "ㄗ", "ㄘ",
-    "ㄙ"
-]);
-
-const 介音集 = new Set<string>([
-    "ㄧ", "ㄨ", "ㄩ"
-]);
-
-const 韻母集 = new Set<string>([
-    "ㄚ", "ㄛ", "ㄜ", "ㄝ", "ㄞ",
-    "ㄟ", "ㄠ", "ㄡ", "ㄢ", "ㄣ",
-    "ㄤ", "ㄥ", "ㄦ"
-]);
-
-const 聲調集 = new Set<string>([
-    "ˊ", "ˇ", "ˋ", "˙"
-]);
-
-function 字串轉注音種類列舉(s: string): 注音種類列舉 {
-    if (聲母集.has(s)) { return 注音種類列舉.聲母; }
-    if (介音集.has(s)) { return 注音種類列舉.介音; }
-    if (韻母集.has(s)) { return 注音種類列舉.韻母; }
-    if (聲調集.has(s)) { return 注音種類列舉.聲調; }
-    return null;
-}
-
-// TODO: 檢查不合法
-function 字串轉注音表示類(s: string): 注音表示類 {
-    let ret = new 注音表示類();
-    for (let c of s) {
-        switch (字串轉注音種類列舉(c)) {
-            case 注音種類列舉.聲母: {
-                ret.聲母 = c;
-                break;
-            }
-            case 注音種類列舉.介音: {
-                ret.介音 = c;
-                break;
-            }
-            case 注音種類列舉.韻母: {
-                ret.韻母 = c;
-                break;
-            }
-            case 注音種類列舉.聲調: {
-                ret.聲調 = toneCharToEnum(c);
-                break;
-            }
-            default: {
-                throw Error(`${c} 並非注音符號`)
-            }
-        }
-    }
-    return ret;
-}
-
-function isValidBopomofo(s: string): boolean {
-    try {
-        字串轉注音表示類(s);
-        return true;
-    } catch (error) {
-        // console.error(error);
-        return false;
-    }
-}
-
-// 發現原字典檔中，輕聲的符號會寫在前面，其他聲調則寫在後面，故需要正規化
-function normalizeBopomofo(s: string): string {
-    return 字串轉注音表示類(s).toString();
-}
-
-class Dictionary {
+// TODO: 以 trie 樹實作 getBypartialBopomofo
+export class Dictionary {
     bopomofoToChar: Map<string, string[]>;
+    charSet: Set<string>;
     constructor() {
         this.bopomofoToChar = new Map<string, string[]>();
+        this.charSet = new Set<string>();
+    }
+    containChar(c: string): boolean {
+        return this.charSet.has(c);
     }
     getByCompleteBopomofo(bopomofo: string): string[] {
         let ret = this.bopomofoToChar.get(bopomofo);
@@ -87,7 +22,7 @@ class Dictionary {
     }
 }
 
-function getDictionary(): Dictionary {
+export function getDictionary(): Dictionary {
     let dictFile = fs.readFileSync("./data/dict-revised.unicode.json").toString();
     let dictJSON = JSON.parse(dictFile);
     // 過濾長度以得到「字典」而非原來的「詞典」
@@ -118,6 +53,7 @@ function getDictionary(): Dictionary {
     let dictionary = new Dictionary();
 
     for(let w of tmpDict) {
+        dictionary.charSet.add(w.char);
         for(let b of w.bopomofo) {
             if(isValidBopomofo(b)) {
                 let nb = normalizeBopomofo(b);
@@ -139,4 +75,4 @@ function getDictionary(): Dictionary {
 
 // let dict = getDictionary();
 
-// console.log(dict);
+// console.log(dict.charSet.size);
