40bc09f0fecd94375ce74d7109c0ec8ccf870960
davidyo <lturtsamuel@gmail.com>
2019-01-05 22:52:07 +0800

    至少可以train RNN了 (訓練誤差小得離譜)

--- RNN/char_db.py
+++ RNN/char_db.py
@@ -6,10 +6,12 @@ import numpy as np
 class CharDB:
     def __init__(self, char_file_name):
         self.char_file_name = char_file_name
-        self.article_file_names = []
+        self.article = ""
         self.genBopomofoDict()
-    def setArticleName(self, filename):
-        self.article_file_names.append(filename)
+    def readArticle(self, filename):
+        with open(filename) as f:
+            self.article += f.read() + "\n"
+
     def genBopomofoDict(self):
         json_data = None
         with open(self.char_file_name) as f:
@@ -30,12 +32,10 @@ class CharDB:
 
     def genWordEmbedding(self):
         array_ch = []
-        for article_file_name in self.article_file_names:
-            with open(article_file_name) as f:
-                s = f.read()
-                for ch in s:
-                    if(ch in self.bopomofo_dict):
-                        array_ch.append(ch)
+        for ch in self.article:
+            if(ch in self.bopomofo_dict):
+                array_ch.append(ch)
+
         print("Word Embedding START")
         we_model = Word2Vec(array_ch, min_count=10, size=30, workers=4)
         print("Word Embedding DONE")
@@ -46,7 +46,7 @@ class CharDB:
         for i, ch_item in enumerate(ch_items):
             ch = ch_item[0]
             self.ch_id_dict[ch] = i + 2
-            self.we_mat[i + 1, :] = we_model.wv[ch]
+            self.we_mat[i + 2, :] = we_model.wv[ch]
     
     def saveWE(self, dict_name="ch_id_dict.npy", mat_name="we_mat.npy"):
         np.save(dict_name, self.ch_id_dict)
@@ -56,10 +56,46 @@ class CharDB:
         self.ch_id_dict = np.load(dict_name).item()
         self.we_mat = np.load(mat_name)
 
+    def chars2XandY(self, s, arr_len=5):
+        mat_X = []
+        mat_Y = []
+        for line in str.split(s, "\n"):
+            #print(line)
+            front = -arr_len
+            while(front + arr_len < len(line)):
+                if(not line[front + arr_len] in self.ch_id_dict):
+                    line = line[front + arr_len + 1:]
+                    front = -arr_len
+                    continue
+                arr_idx = []
+                mat_Y.append(self.ch_id_dict[line[front + arr_len]])
+                for i in range(front, front + arr_len):
+                    idx, ch = self.ch_id_dict["__unknown__"], None
+                    if(i < 0):
+                        idx = self.ch_id_dict["__pad__"]
+                    elif(line[i] in self.ch_id_dict):
+                        idx = self.ch_id_dict[line[i]]
+                        #print(line[i])
+                    arr_idx.append(idx)
+                #print("Ans: " + line[front + arr_len])
+                #print("====")
+                mat_X.append(arr_idx)
+                front += 1
+        return np.matrix(mat_X), self.idx2OneHot(mat_Y)
+    
+    def idx2OneHot(self, mat_idx):
+        ch_count = self.getCharCount()
+        one_hot = np.zeros((len(mat_idx), ch_count))
+        for i, idx in enumerate(mat_idx):
+            one_hot[i, idx] = 1
+        return one_hot
+        
+    def getCharCount(self):
+        return len(self.we_mat)
 
 if __name__ == "__main__":
     db = CharDB("../data/dict-revised.unicode.json")
-    db.setArticleName("../data/train_x.csv")
-    db.setArticleName("../data/test_x.csv")
+    db.readArticle("../data/train_x.csv")
+    db.readArticle("../data/test_x.csv")
     db.genWordEmbedding()
     db.saveWE()
--- RNN/rnn.py
+++ RNN/rnn.py
@@ -1,4 +1,45 @@
+import tensorflow as tf
+import numpy as np
+
+from keras.layers import Dense, Dropout, Activation , Flatten, TimeDistributed, AveragePooling1D
+from keras.layers import Bidirectional
+from keras.models import Sequential
+from keras.preprocessing import sequence
+from keras.layers import Embedding, LSTM, GRU, SimpleRNN
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import EarlyStopping,ModelCheckpoint
+from keras import optimizers
+from keras import regularizers
+from keras.utils import np_utils
+import keras.callbacks
+
 from char_db import CharDB
 
 char_db = CharDB("../data/dict-revised.unicode.json")
 char_db.loadWE()
+char_db.readArticle("../data/train_x.csv")
+char_db.readArticle("../data/test_x.csv")
+article = char_db.article
+train_X, train_Y = char_db.chars2XandY(article[:len(article)//3])
+val_X, val_Y = char_db.chars2XandY(article[len(article)//3+1:len(article)//3+1000])
+
+with tf.device("/gpu:0"):
+    model = Sequential()
+    model.add(Embedding(len(char_db.we_mat),
+                        30,
+                        weights=[char_db.we_mat],
+                        trainable=True,
+                        input_length=5))
+    model.add(GRU(60, return_sequences=False, dropout=0.3, recurrent_dropout=0.3))
+    model.add(Dense(units=char_db.getCharCount(), activation='relu'))
+    model.add(Dense(char_db.getCharCount(), activation='softmax'))
+    model.summary()
+
+    model.compile(loss='binary_crossentropy',
+                       optimizer= 'adam',
+                       metrics=(['accuracy']))
+    
+    early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=0, mode="auto")
+    checkpt = ModelCheckpoint("ckpt_model.h5", monitor="val_acc", verbose=0, save_best_only=True, save_weights_only=False, mode="auto", period=1)
+    history = model.fit(train_X, train_Y,
+              batch_size=1024, epochs=50, validation_data=(val_X, val_Y), callbacks=[early_stopping,checkpt])
