12e499284e41dea0d722ff699222503f2cd6eb75
DavidYo <lturtsamuel@gmail.com>
2019-01-07 02:50:40 +0800

    可以自動測試(hmm)，但必需先處理查不到字噴錯誤的狀況

--- grouping_kernel.ts
+++ grouping_kernel.ts
@@ -10,9 +10,14 @@ export class GroupingKernel {
 
         let prev_type: 注音種類列舉 = 注音種類列舉.無;
         for(let input_unit of input_unit_list) {
+            if(!input_unit) {
+                continue;
+            }
             if(聲調集.has(input_unit.值)) {
                 cur_select_unit.值.聲調 = toneCharToEnum(input_unit.值);
-                select_unit_list.push(cur_select_unit);
+                if(cur_select_unit.值.介音 || cur_select_unit.值.韻母 || cur_select_unit.值.聲母) {
+                    select_unit_list.push(cur_select_unit);
+                }
                 prev_type = 注音種類列舉.聲調;
 
                 cur_select_unit = new 選字單元();
@@ -23,9 +28,9 @@ export class GroupingKernel {
                     cur_select_unit.值.介音 = input_unit.值;
                 } else if(韻母集.has(input_unit.值)) {
                     cur_select_unit.值.韻母 = input_unit.值;
-                } else {
+                } else if(聲母集.has(input_unit.值)) {
                     cur_select_unit.值.聲母 = input_unit.值;
-                }               
+                }
             }
         }
         return select_unit_list;
--- testing/testing.ts
+++ testing/testing.ts
@@ -1,22 +1,41 @@
-import { HMMKernel } from "../HMM";
 import fs from "fs";
+
+import { HMMKernel, getSentences } from "../HMM"
+import { getDictionary, Dictionary } from "../pre_process"
+import { 注音表示類, 選字單元, 輸入單元, 輸入單元種類, GroupMethod } from "../basic";
+import { GroupingKernel } from "../grouping_kernel";
 
 const bopomofo_filename = process.argv[2];
 const ans_filename = process.argv[3];
 
-function predictAll(string_bopomofo) {
-    for(let bofomofo_seq of string_bopomofo.split(",")) {
+const dictionary = getDictionary();
+const sentences = getSentences("./downloader", dictionary);
+const kernel = new HMMKernel(dictionary, sentences);
 
+function predictAll(string_bopomofo) {
+    let array_predict = [];
+    console.log(JSON.stringify(string_bopomofo.slice(0, 20)))
+    for(let bopomofo_seq of string_bopomofo.split(/, */)) {
+        let input_unit_list = bopomofo_seq.split("").map(bopomofo => {
+            if(bopomofo.length != 1) {
+                return null;
+            }
+            let unit = new 輸入單元();
+            unit.種類 = 輸入單元種類.注音;
+            unit.值 = bopomofo;
+            return unit;
+        });
+        let group_list = GroupingKernel.assignGroup(input_unit_list);
+        array_predict.push(kernel.determineSequence(group_list, GroupMethod.complete));
     }
+    return array_predict;
 }
 
 fs.readFile(ans_filename, (err, data) => {
     let string_ans = data.toString();
     fs.readFile(bopomofo_filename, (err, data) => {
         let string_bopomofo = data.toString();
-        let string_predict = predictAll(string_bopomofo);
+        let array_predict = predictAll(string_bopomofo);
     });
 
 });
-
-HMMKernel.determineSequence: (input: Array<選字單元>, groupMethod: GroupMethod) => Array<string>;
